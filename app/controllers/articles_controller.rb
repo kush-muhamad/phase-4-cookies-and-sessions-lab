77923600e73c3050315b31bec49b80b1f86fd9d3
kush Muhamad <kushmohammed98@yahoo.com>
2023-07-01 13:20:27 +0300

    completed task

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    session[:page_views] ||= 0
+    if session[:page_views].to_i < 3
+      session[:page_views] = session[:page_views].to_i + 1
     article = Article.find(params[:id])
     render json: article
+    else
+      render json: { error: "You need to be logged in to view extra pages" }, status: :unauthorized
   end
+end
 
   private
 
